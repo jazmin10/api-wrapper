aff5a44982345e53aa0138730f721fc2043119d3
jazmin10 <jazminestrada10@gmail.com>
2019-09-28 15:06:49 -0500

    add getBaseUrl to Backend class (returns base url of API)

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ class Backend {
   setBaseUrl(baseUrl) {
   	this.baseUrl = baseUrl;
   }
+
+  getBaseUrl() {
+  	return this.baseUrl;
+  }
 }
 
 
 //sample usage
 const API = new Backend();
 API.setBaseUrl("https://api.learnjavascript.online/demo");
-// console.log(API.getBaseUrl());
+console.log(API.getBaseUrl());
 
 // API.get("/notifications/new.json")
 // .then(data => {
