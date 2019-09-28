557926545dad9dbb57178e0e5d51281b42616d64
jazmin10 <jazminestrada10@gmail.com>
2019-09-28 14:59:28 -0500

    initial files

---
+++ index.js
@@ -0,0 +1,24 @@
+class Backend {
+  //do not modify the constructor
+  constructor(){
+      this.baseUrl = '';
+  }
+}
+
+
+//sample usage
+const API = new Backend();
+API.setBaseUrl("https://api.learnjavascript.online/demo");
+console.log(API.getBaseUrl());
+
+API.get("/notifications/new.json")
+.then(data => {
+    console.log(data.count);
+});
+
+API.post("/food.json", {
+    food: "Pasta"
+})
+.then(data => {
+    console.log(data);
+});
